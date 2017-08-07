c6e668873e3246705fa52089c21c92f6835e02a4
Amoury <ansar.memon@hotmail.com>
2017-08-07 15:09:57 +0400

    PPT Ready

--- src/js/ArChart.js
+++ src/js/ArChart.js
@@ -6,12 +6,12 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو"],
         datasets: [{
             label: "Invoices Generated",
             borderColor: 'rgb(255, 99, 132)',
             backgroundColor: 'transparent',
-            data: [50, 85, 125, 110, 75, 95, 145],
+            data: [50,85,125,110,75,95,145].toLocaleString('ar-EG')
         }]
     },
 
@@ -80,57 +80,6 @@ var customerGrowthChart = new Chart(customerGrowth,{
 		}
 });
 
+var number = 3500;
 
-$(document).ready(function() {
-    $('.delete-button').on('click', function(e) {
-        e.preventDefault();
-        $( this ).parent().fadeOut( "slow", function() {
-            this.remove();
-        });
-    });
-
-    $('.side__nav-item').on('click', function(e) {
-        e.preventDefault();
-        $('.side__nav-item').removeClass("menu-active");
-        $( this ).addClass("menu-active");
-    });
-});
-
-
-function showPassword(obj) {
-    var obj = document.getElementById('passwordInput');
-    if(obj.type == "password") {
-        obj.type = "text";
-        $('.showPassword').removeClass('fa-eye').addClass('fa-eye-slash');
-    } else {
-        obj.type = "password";
-        $('.showPassword').removeClass('fa-eye-slash').addClass('fa-eye');
-    }
-}
-
-
-function getFormValues(event) {
-	var username = $("#username").val();
-	var password = $("#passwordInput").val();
-	var href = $('#submit-btn').attr('href');
-
-	event.preventDefault();
-
-	if (username == "admin" && password == "admin123") {
-		if($('body').attr('dir') == 'rtl') {
-			window.location.replace("http://localhost:3000/ar/dashboard.html")
-		} else {
-			window.location.replace("http://localhost:3000/dashboard.html");
-		}
-	} else {
-		$('#username').css("border-bottom-color","red");
-		$('#passwordInput').css("border-bottom-color","red");
-		$('form').effect("shake");
-	}	
-}
-
-
-	// $('.submit-btn').on('click', function(e) {
-	// 	e.preventDefault;
-	// 	console.log("YES");
-	// });
+console.log(number.toLocaleString('ar-EG'));
